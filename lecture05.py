12d4b4e24d5f7b98bd6aea250337074a47f2942f
AChicaiza10 <143219054+AChicaiza10@users.noreply.github.com>
2023-09-12 12:09:06 -0400

    Lecture 05

---
+++ lecture05.py
@@ -0,0 +1,82 @@
+%# coding: utf-8
+print("Hello World")
+foo=["a","b"]
+get_ipython().run_line_magic('save', 'mycode')
+foo2=["b","a"]]
+foo2=["b","a"]
+foo2==foo
+foo2.sort()
+foo2
+foo2==foo
+animals={'dog','cat'}
+type(animals)
+animals2={'cat','dog'}
+animals2
+animals==animals2
+animals.add('turle')
+animals
+animals2.add('fish')
+animals2
+animals.union(animals2)
+print(animals | animals2)
+animals.intersection(animals2)
+print(animals & animals2)
+animals3={'cow'}
+animals2.intersection(animals3)
+set(['a','b'])
+list(set(['a','b']))
+list(set(['a','b','b','c']))
+animal_legs={'dog':4, 'ant':6}
+print(animal_legs)
+aniamls_leg[dog]
+animal_legs[dog]
+print(animal_legs)
+animal_legs['dog']
+animal_legs['spider']
+animals_legs['spider']=8
+animal_legs['spider']=8
+animal_legs['spider']
+set(animal_legs.key())
+set(animal_legs.keys())
+set(animal_legs.values())
+animal_legs.values
+animal_legs.values()
+animals
+legs=[4,4,4]
+animal_legs_data=[('cat',4),('dog',4),('turtle',4)]
+dict(animal_legs_data)
+zip(animals,legs)
+list(zip(animals,legs))
+dict(list(zip(animals,legs)))
+dict(list(animals,legs))
+dict(zip(animals,legs))
+animals_legs.values
+animal_legs.value()
+animal_legs.items()
+animal_legs.values()
+for thing in animal_legs:
+    print('thing')
+    
+for thing in animal_legs:
+    print(thing)
+    
+for thing in animal_legs:
+    print("animal:",thing,"value:",animal_legs[thing])
+    
+for thing in animal_legs:
+    print("animal:",thing,"legs:",animal_legs[thing])
+    
+for key, value in animal_legs.item():
+    print("animal;",key,"legs:",value)
+    
+for key, value in animal_legs.items():
+    print("animal;",key,"legs:",value)
+    
+for key, value in animal_legs.items():
+    print("animal:",key,"legs:",value)
+    
+data = [{'animal':'dog','legs':,4},{'animal':'cat','legs':4}]
+data = [{'animal':'dog','legs':4},{'animal':'cat','legs':4}]
+data
+data2={'animals':['dog','cat','bob'],'legs':[4,4,2]}
+data2
